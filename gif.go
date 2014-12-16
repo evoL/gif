351582c0b379eabb381bda51a03d649dc63c8cca
Rafał Hirsz <rafal@hirsz.co>
2014-12-17 00:42:10 +0100

    Used cli to make a proper CLI tool

--- gif.go
+++ gif.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/codegangsta/cli"
+	"os"
+)
+
 func main() {
-	HelloWorld()
+	app := cli.NewApp()
+	app.Name = "gif"
+	app.Usage = "a stupid gif manager"
+	app.Action = func(c *cli.Context) {
+		HelloWorld()
+	}
+
+	app.Run(os.Args)
 }
