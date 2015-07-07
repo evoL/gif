eeafab512d59e541cd9b70561d9cd210ad8a7d35
Rafał Hirsz <rafal@hirsz.co>
2015-07-07 23:41:47 +0200

    MOAR BASH MISTAKES.

--- setup_darwin.sh
+++ setup_darwin.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -ev
 
 if [ "$GIF_OS" == "darwin" ]; then
@@ -6,9 +6,8 @@ if [ "$GIF_OS" == "darwin" ]; then
   git clone https://github.com/tpoechtrager/osxcross /tmp/osxcross
   cd /tmp/osxcross
 
-  # Download prepared SDKs
+  # Download prepared SDK
   wget --quiet -O tarballs/MacOSX10.10.sdk.tar.xz https://dl.dropboxusercontent.com/u/2078673/osx-sdks/MacOSX10.10.sdk.tar.xz
-  wget --quiet -O tarballs/MacOSX10.9.sdk.tar.xz https://dl.dropboxusercontent.com/u/2078673/osx-sdks/MacOSX10.9.sdk.tar.xz
 
   # Build the toolchain
   UNATTENDED=1 SDK_VERSION=10.10 OSX_VERSION_MIN=10.9 ./build.sh
